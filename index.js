22c12b0b18e1403d9fcf01b621fa26134c145f25
mgrinko <glukym@gmail.com>
2020-01-31 11:45:46 +0200

    Port was added for heroku

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use((req, res, next) => {
   res.set('Access-Control-Allow-Origin', '*');
